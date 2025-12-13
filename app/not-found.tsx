dc7f2726d17380e9743e57d674a73eec24df46c5
jealife <jeanguylanememiaghe@gmail.com>
2025-12-13 17:35:08 +0100

    not foun review

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function NotFoundPage() {
+  return (
+    <main className=" flex items-center justify-center text-center py-24">
+      <div className='flex flex-col items-center justify-center text-center '>
+        <h1 className="text-3xl font-bold">Page introuvable</h1>
+        <p className="text-gray-500 mt-2">
+          Cette page n’existe pas.
+        </p>
+        <Image width={500} height={500} src="/404-error.png" className='w-[65%] md:w-[80%]' alt="" />
+      </div>
+    </main>
+  );
+}
