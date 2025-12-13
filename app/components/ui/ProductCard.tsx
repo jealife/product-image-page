80ed91468ce8b0f741c72311e5380e0982a3ae1b
jealife <jeanguylanememiaghe@gmail.com>
2025-12-13 16:56:18 +0100

    shop link update

--- app/components/ui/ProductCard.tsx
+++ app/components/ui/ProductCard.tsx
@@ -29,26 +29,26 @@ export default function ProductCard({ product }: ProductCardProps) {
     //     </div>
     //   </div>
     // </Link> 
-    <article className="product_card md:h-[430px] h-[180px] relative group block bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300   w-full overflow-clip card card">
-      {/* //</article><img aria-label="Nike Air Force 1" src="https://images.unsplash.com/photo-1626379616459-b2ce1d9decbc?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Nike Air Force 1" className="image_product"> --> */}
-      <img aria-label={`Image de ${name}`}
-        src={imageUrl}
-        alt={`Image de ${name}`}
-        className="image_product" />
-      <div className="card_content_product md:p-4 px-2 py-3 relative">
-        <span className="product text-gray-700">
-          <h3 className="product_name font-normal text-xs md:text-lg">{name}</h3>
-          <span className="price_product font-bold text-sm md:text-lg">${price.toFixed(2)}</span>
-        </span>
-        <Link href={`/products/${slug}`} className="btn_product cursor-pointer  bg-black/35 text-white font-semibold   transition-colors duration-300">
-          <button >
-            Buy Now
-          </button>
-        </Link>
-        <span className='py-1 px-2 flex items-center justify-center rounded-2xl absolute top-4 left-4 bg-black/35 text-white font-semibold   transition-colors duration-300'>
-          <p className="md:text-xs text-[10px] text-gray-100 font-medium uppercase">{brand}</p>
-        </span>
-      </div>
-    </article>
+      <Link href={`/products/${slug}`} className="product_card md:h-[430px] h-[180px] relative group block bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300   w-full overflow-clip card card">
+        {/* //</article><img aria-label="Nike Air Force 1" src="https://images.unsplash.com/photo-1626379616459-b2ce1d9decbc?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Nike Air Force 1" className="image_product"> --> */}
+        <img aria-label={`Image de ${name}`}
+          src={imageUrl}
+          alt={`Image de ${name}`}
+          className="image_product" />
+        <div className="card_content_product md:p-4 px-2 py-3 relative">
+          <span className="product text-gray-700">
+            <h3 className="product_name font-normal text-xs md:text-lg">{name}</h3>
+            <span className="price_product font-bold text-sm md:text-lg">${price.toFixed(2)}</span>
+          </span>
+          {/* <Link href={`/products/${slug}`} className=" hidden md btn_product cursor-pointer  bg-black/35 text-white font-semibold   transition-colors duration-300">
+            <button >
+              Buy Now
+            </button>
+          </Link> */}
+          <span className='py-1 px-2 flex items-center justify-center rounded-2xl absolute top-4 left-4 bg-black/35 text-white font-semibold   transition-colors duration-300'>
+            <p className="md:text-xs text-[10px] text-gray-100 font-medium uppercase">{brand}</p>
+          </span>
+        </div>
+      </Link>
   );
 }
